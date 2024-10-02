2cbb8a56d124216052a2e1d3937b4816f9ee82ea
cr1m3 <jeffryjulianto7@gmail.com>
2024-10-02 23:38:22 +0700

    Delete js/hide.js

--- js/hide.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("myButton").addEventListener("click", function() {
-    var myContent = document.getElementById("myContent");
-    if (myContent.style.display === "none") {
-        myContent.style.display = "block";
-    } else {
-        myContent.style.display = "none";
-    }
-});
