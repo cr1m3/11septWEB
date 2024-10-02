5b751f5b9574ba6462b47e3459ddcb222bb36194
cr1m3 <jeffryjulianto7@gmail.com>
2024-10-02 21:59:17 +0700

    Update hide.js

--- js/hide.js
+++ js/hide.js
@@ -1,7 +1,7 @@
 const myButton = document.getElementById("myButton");
 const myImg = document.getElementById("myImg");
 
-myButton.addEventListener("click", event => {
+myButton.addEventListener("click", event => { // eslint-disable-line no-unused-vars
 
     if(myImg.style.visibility === "hidden"){
         myImg.style.visibility = "visible";
